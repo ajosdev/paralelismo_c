e4cd21a73c34ac61a092ff47ac05c1746ac34683
Anderson Silva <ajos.dev@gmail.com>
2019-10-28 16:52:18 -0300

    28/10 16:38 ex4_

---
+++ parall_primos_dinamyc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+int ehPrimo(long num) {
+   long divisor;
+
+   if( num <= 1 )
+    return 0;
+   else
+    if(num > 3){
+      if(num % 2 == 0)
+        return 0;
+      long max_divisor = sqrt(num);
+      for(divisor = 3; divisor <= max_divisor; divisor += 2)
+	    if (num % divisor == 0)
+	       return 0;
+    }
+   return 1;
+}
+
+int main() {
+  long LIMITE_MAX = 2000000;
+  long contaPrimo;
+  long soma;
+  int n;
+double start = omp_get_wtime();
+
+  if (LIMITE_MAX <= 1)
+    soma = 0;
+  else {
+    soma = 1;
+    #pragma omp parallel for private (cont_primo) reduction(+:soma) schedule (dynamic,100)
+    for (n = 3; n < LIMITE_MAX; n += 2){
+      contaPrimo = ehPrimo(n);
+      soma = soma + contaPrimo;
+    }
+  }
+printf("time: %.15lf \n", omp_get_wtime()-start);
+  printf("Número total de primos: %ld\n", soma);
+
+  return 0;
+}
+
